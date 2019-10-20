9975d26ba8f4e9c0418d773de357bb9d84ce2d95
Cicy29 <47091978+Cicy29@users.noreply.github.com>
2019-10-20 18:45:54 +0200

    Add files via upload

--- JSWeek1/index.js
+++ JSWeek1/index.js
@@ -1,17 +1,14 @@
-// This app connects music lovers and nostalgics of vinyls. 
+// This app connects music lovers and nostalgics of vinyls.
 // This app allows users to be a part of a community where they can exchange, sell or buy vinyls they may not find in stores."
 
-Seller = class {
-  constructor(name, age, location, artist, album, price) {
+User = class {
+  constructor(name, age, location) {
     this.userName = name;
     this.age = age;
     this.location = location;
-    this.vinylArtist = artist;
-    this.albumName = album;
-    this.price = price;
   }
-  welcome(seller) {
-    "Welcome to WonderVinyl " + seller.name + " !";
+  welcome(user) {
+    console.log("Welcome to WonderVinyl " + user.name + " !");
   }
 
   sellRecord(vinyl) {
@@ -19,73 +16,50 @@ Seller = class {
     this.vinyl = vinyl;
     this.records.push(this);
   }
-};
 
-Purchaser = class {
-  constructor(name, age, location, artist, album, price) {
-    this.userName = name;
-    this.age = age;
-    this.location = location;
-    this.vinylArtist = artist;
-    this.albumName = album;
-    this.price = price;
-  }
-  welcome(Purchaser) {
-    "Welcome to WonderVinyl " + purchaser.name + " !";
-  }
   buyRecord(vinyl) {
     console.log("Thank you for your purchase. Discover what we have in store!");
     this.vinyl = vinyl;
     this.records.push(this);
   }
-};
 
-Trader = class {
-  constructor(name, location, artist, album) {
-    this.userName = name;
-    this.location = location;
-    this.vinylArtist = artist;
-    this.albumName = album;
-  }
-  welcome(trader) {
-    "Welcome to WonderVinyl " + trader.name + " !";
-  }
   exchangeRecord(vinyl) {
     console.log("Excellent! Keep trading!");
     this.vinyl = vinyl;
     this.records.push(this);
   }
-  
 };
 
-  Vinyl = class {
-    constructor(name, title, date, genre) {
-      this.nameArtist = name;
-      this.titleAlbum = title;
-      this.releaseDate = date;
-      this.genre = genre;
-      this.records = [];
+Vinyl = class {
+  constructor(name, title, date, genre, price) {
+    this.nameArtist = name;
+    this.titleAlbum = title;
+    this.releaseDate = date;
+    this.genre = genre;
+    this.price = price;
+    if (this.priceNotAvailable) {
+      console.log("Please select Exchange option.");
     }
-  };
-
-  listOfAllRecordsAvailable() {
-    this.records.forEach(printAllRecords);
-
-    printAllRecords = vinyl => console.log(vinyl.nameArtist);
-
-    amyWinehouse = new Vinyl("Amy Winehouse", "Back to Black", 2006, "Soul");
-    nirvana = new Vinyl("Nirvana", "Nevermind", 1991, "Rock");
+    this.records = [];
   }
 };
 
-clark = new Seller("Clark Kent",27,"Berlin","Amy Winehouse","Back to Black",10);
-selina = new Purchaser("Selina Kyle", 32, "Berlin", "Nirvana", "Nevermind", 8);
-selina = new Trader("Jean Grey",32,"Berlin","Celia Cruz","Mi vida es cantar");
-bruce = new Trader("Bruce Wayne", 32, "Berlin", "David Bowie", "Aladin Sane");
+printAllRecordsAvailable();
+this.records.forEach(printAllRecords);
 
-shuri = new Trader("Shuri Panther", 32, "Berlin", "Rihanna", "Loud");
+printAllRecords = vinyl => console.log(vinyl.nameArtist);
 
+amyWinehouse = new Vinyl("Amy Winehouse", "Back to Black", 2006, "Soul");
+nirvana = new Vinyl("Nirvana", "Nevermind", 1991, "Rock");
 rihanna = new Vinyl("Rihanna", "Loud", 2010, "Pop");
+davidBowie = new Vinyl("David Bowie", "Aladin Sane", 1973, "Pop rock");
+
+clark = new User("Clark Kent", 27, "Berlin");
+selina = new User("Selina Kyle", 22, "Berlin");
+selina = new User("Jean Grey", 32, "Berlin");
+bruce = new User("Bruce Wayne", 44, "Berlin");
+shuri = new User("Shuri Panther", 18, "Berlin");
 
 clark.exchangeRecord("Rihanna");
 shuri.buyRecord("Nirvana");
+bruce.sellRecord("David Bowie");
