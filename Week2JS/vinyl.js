91de71ff18d280ec10c789ba45be3e038677b211
Cicy29 <47091978+Cicy29@users.noreply.github.com>
2019-10-29 21:34:19 +0100

    Update vinyl.js

--- Week2JS/vinyl.js
+++ Week2JS/vinyl.js
@@ -1,15 +1,16 @@
 // const Chalk = require('chalk')
 module.exports = class Vinyl {
     constructor(name, title, date, genre, price) {
-        // if (!price) {
-        //     throw new Error("Please select Exchange option.");
-        // }
+        if (!price) {
+            throw new Error("Please select Exchange option.");
+        }
         this.nameArtist = name;
         this.titleAlbum = title;
         this.releaseDate = date;
         this.genre = genre;
         this.price = price;
         this.records = [];
+        this.user = [];
 
     }
 
